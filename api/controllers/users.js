1a4b09bccf4989bf136548c781b9c242072fa897
ivan-cardoso <ivancardoso59@gmail.com>
2022-05-06 12:07:00 -0300

    SigUp Implementation - SignIn advances

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -2,25 +2,40 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const { User } = require('../models/index')
 
-const signIn = (req, res) => {
+const signIn = async (req, res) => {
+    const { email, password } = req.body
 
+    try {
+        if (!email || !password) return res.status(400).json({ 'message': 'Invalid inputs' })
+
+        const user = await User.findOne({email})
+        if(!user) return res.status(401).json({ "message": "User doesn't exist" })
+
+        // const isPasswordCorrect = bc
+    } catch (error) {
+
+    }
 }
 
 const signUp = async (req, res) => {
     const { name, email, password } = req.body
     try {
-        const user = await User.findOne({email})
-        if (user) return res.status(400).json({ 'message': 'User already exist' })
+        if (!name || !email || !password) return res.status(400).json({ 'message': 'Invalid inputs' })
+
+        const user = await User.findOne({ email })
+        if (user) return res.status(401).json({ 'message': 'User already exist' })
 
         const hashedPassword = await bcrypt.hash(password, 12)
 
         const newUser = await User.create({
-            "name" : name,
-            "email" : email,
+            "name": name,
+            "email": email,
             "password": hashedPassword
         })
 
-        res.status(201).json(newUser)
+        const token = jwt.sign({ "email": newUser.email, "id": newUser._id }, 'TEST', { "expiresIn": "1h" })
+
+        res.status(201).json({ newUser, token })
 
     } catch (error) {
         res.status(500).json(error)
