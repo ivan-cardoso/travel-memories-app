ae16bf8066b0384a8ede8224fa7b5c9ba207a907
ivan-cardoso <ivancardoso59@gmail.com>
2022-05-09 10:00:39 -0300

    LogIn Implementation - Front folders

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -9,11 +9,17 @@ const signIn = async (req, res) => {
         if (!email || !password) return res.status(400).json({ 'message': 'Invalid inputs' })
 
         const user = await User.findOne({email})
-        if(!user) return res.status(401).json({ "message": "User doesn't exist" })
+        if(!user) return res.status(404).json({ "message": "User doesn't exist" })
 
-        // const isPasswordCorrect = bc
+        const isPasswordCorrect = await bcrypt.compare(password, user.password)
+
+        if(!isPasswordCorrect) return res.status(401).json({'message' : 'Bad credentials'})
+
+        const token = jwt.sign({"email" : user.email, "id" : user._id}, 'TEST', {"expiresIn" : '1h'})
+        
+        res.status(200).json({user, token})
     } catch (error) {
-
+        res.status(500).json(error)
     }
 }
 
